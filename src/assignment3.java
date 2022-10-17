815487dbb0c00d0ee49ce8ab37ae5b8eeb802e11
Divya-Khare-TeknistTestare2022 <divya.24.khare@gamil.com>
2022-10-17 13:52:43 +0200

    Java program

---
+++ src/assignment3.java
@@ -0,0 +1,16 @@
+public class assignment3 {
+
+    public static void main(String[] args) {
+        //Finding leep year
+        //Current year: 2022
+
+int year = 2022;
+
+if (int year % 4  )
+)
+    System.out.println("The year is a leap year");
+
+    else (The year is not a leap year)
+
+
+    }
---
+++ src/assignmentJava.java
@@ -0,0 +1,16 @@
+public class assignmentJava {
+
+    public static void main(String[] args) {
+        //Finding leep year
+        //Current year: 2022
+
+int year = 2022;
+
+if (int year % 4 ==0 );
+)
+    System.out.println("The year is a leap year");
+
+    else ("The year is not a leap year");
+
+
+    }
